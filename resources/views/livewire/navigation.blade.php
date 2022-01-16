aac003c604d472d5d508810812ad71d9bd3e1ef0
jmdb <juan.manuel.delgado.barranco@gmail.com>
2022-01-16 21:23:05 +0100

    Pdf 6: Despliegue de categorias y subcategorias (No funciona bien por el $category->icon)

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,9 +1,10 @@
 <header class="bg-trueGray-700 sticky top-0" x-data="dropdown()">
     <div class="container-menu flex items-center h-16">
-        <a :class="{'bg-opacity-100 text-orange-500': open}" x-on:click="show()" class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
+        <a :class="{'bg-opacity-100 text-orange-500': open}"
+           x-on:click="show()"
+           class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold hfull">
             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4
-12h16M4 18h16" />
+                <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M412h16M4 18h16" />
             </svg>
             <span class="text-sm">Categorías</span>
         </a>
@@ -72,20 +73,19 @@
         </div>
         @livewire('dropdown-cart')
     </div>
-    <nav id="navigation-menu" x-show="open"
-         :class="{'block': open, 'hidden': !open}" x-show="open"  class="bg-trueGray-700 bg-opacity-25 w-full absolute hidden">
+    <nav id="navigation-menu" x-show="open" :class="{'block': open, 'hidden': !open}" class="bg-trueGray-700 bg-opacity-25 w-full absolute hidden">
         <div class="container-menu h-full">
-            <div x-on:click.away="show()" class="grid grid-cols-4 h-full relative">
+            <div x-on:click.away="close()" class="grid grid-cols-4 h-full relative">
                 <ul class="bg-white">
-                    @foreach($categories as $category)
-                        <li class="navigation-link text-trueGray-500 hover:bg-orange-500 hover:text-white">
+                    @foreach($categories as $category )
+                        <li class="navigation-link text-gray-500 hover:bg-orange-500 hover:text-white">
                             <a href="" class="py-2 px-4 text-sm flex items-center">
-                             <span class="flex justify-center w-9">
-                             {!! $category->icon !!}
-                             </span>
-                                {{ $category->name }}
+                                <span class="flex justify-center w-8">
+                                    {{--{!! $category->icon !!}--}}.......
+                                </span>
+                                {{$category->name}}
                             </a>
-                            <div class="navigation-submenu bg-gray-100 absolute w-3/4 h-full top-0 right-0 hidden">
+                            <div class=" navigation-submenu bg-gray-100 absolute w-3/4 h-full top-0 right-0 hidden" >
                                 <x-navigation-subcategories :category="$category" />
                             </div>
                         </li>
