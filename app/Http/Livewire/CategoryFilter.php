db79258806430c55d056b82f934573d133ae4288
jmdb <juan.manuel.delgado.barranco@gmail.com>
2022-01-19 17:40:38 +0100

    Pdf 9: Categorías

--- app/Http/Livewire/CategoryFilter.php
+++ app/Http/Livewire/CategoryFilter.php
@@ -2,14 +2,41 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class CategoryFilter extends Component
 {
-    public $category;
+    use WithPagination;
+
+    public $category, $subcategoria, $marca;
+
+    public $view = 'grid';
 
     public function render()
     {
-        return view('livewire.category-filter');
+        $productsQuery = Product::query()->whereHas('subcategory.category', function(Builder $query){
+            $query->where('id', $this->category->id);
+        });
+        if ($this->subcategoria) {
+            $productsQuery = $productsQuery->whereHas('subcategory', function(Builder $query){
+                $query->where('name', $this->subcategoria);
+            });
+        }if ($this->marca) {
+        $productsQuery = $productsQuery->whereHas('brand', function(Builder $query){
+            $query->where('name', $this->marca);
+        });
+    }
+
+        $products = $this->category->products()->where('status',2)->paginate(20);
+
+        return view('livewire.category-filter', compact('products'));
+    }
+
+    public function limpiar()
+    {
+        $this->reset(['subcategoria','marca']);
     }
 }
