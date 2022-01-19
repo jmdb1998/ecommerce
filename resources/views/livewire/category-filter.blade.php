8ede17a26a3a7babe0a54be4122375abb8bf9566
jmdb <juan.manuel.delgado.barranco@gmail.com>
2022-01-19 16:19:33 +0100

    Pdf 9: Categorías (a medias)

---
+++ resources/views/livewire/category-filter.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <div class="bg-white rounded-lg shadow-lg mb-4">
+        <div class="px-6 py-2 flex justify-between items-center">
+            <h1>{{ $category->name }}</h1>
+            <div>
+                iconos
+            </div>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
