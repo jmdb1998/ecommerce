bba6de57cf7fef6ed7333da0efe367cf909e12b7
jmdb <juan.manuel.delgado.barranco@gmail.com>
2022-01-21 21:16:21 +0100

    Comprobar que en la vista principal podemos ver al menos 5 productos de una categoría. (no funciona del todo)

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -26,7 +26,7 @@
                     <x-slot name="trigger">
                         <button
                             class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                            <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
+                            <img dusk="profile_image" class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
                                  alt="{{ Auth::user()->name }}"/>
                         </button>
                     </x-slot>
@@ -58,7 +58,7 @@
             @else
                 <x-jet-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <i class="fas fa-user-circle text-white text-3xl cursor-pointer"></i>
+                        <i dusk="not_logged_image" class="fas fa-user-circle text-white text-3xl cursor-pointer"></i>
                     </x-slot>
 
                     <x-slot name="content">
