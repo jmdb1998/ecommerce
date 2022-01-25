3dc443118e9fe654dcdda6ba36cb48a6ec101743
jmdb <juan.manuel.delgado.barranco@gmail.com>
2022-01-25 21:02:46 +0100

    Pdf 18: Crear pedidos 3ª parte

--- resources/views/livewire/create-order.blade.php
+++ resources/views/livewire/create-order.blade.php
@@ -75,7 +75,8 @@
             </div>
         </div>
         <div>
-            <x-jet-button class="mt-6 mb-4" wire:click="create_order">
+            <x-jet-button wire:loading.attr="disabled"
+            wire:target="create_order" class="mt-6 mb-4" wire:click="create_order">
                 Continuar con la compra
             </x-jet-button>
             <hr>
@@ -121,12 +122,22 @@
                 </p>
                 <p class="flex justify-between items-center">
                     Envío
-                    <span class="font-semibold">Gratis</span>
+                    <span class="font-semibold">
+                        @if($envio_type == 1 || $shipping_cost == 0)
+                            Gratis
+                            @else
+                            {{ $shipping_cost }} &euro;
+                        @endif
+                    </span>
                 </p>
                 <hr class="mt-4 mb-3">
                 <p class="flex justify-between items-center font-semibold">
                     <span class="text-lg">Total</span>
+                    @if($envio_type == 1)
                     {{ Cart::subtotal() }} &euro;
+                    @else
+                    {{ Cart::subtotal() + $shipping_cost }} &euro;
+                    @endif
                 </p>
             </div>
         </div>
