753d364ce6c3df4fc2ac8100b3e0744814f1e6e2
jmdb <juan.manuel.delgado.barranco@gmail.com>
2022-01-31 19:37:40 +0100

    Pdf 23: Componente color-product

--- app/Http/Livewire/Admin/ColorProduct.php
+++ app/Http/Livewire/Admin/ColorProduct.php
@@ -2,12 +2,75 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Color;
 use Livewire\Component;
+use App\Models\ColorProduct as TbPivot;
 
 class ColorProduct extends Component
 {
+    protected $rules = [
+        'color_id' => 'required',
+        'quantity' => 'required|numeric'
+    ];
+
+    public $product, $colors;
+    public $color_id, $quantity;
+    public $opne = true;
+    public $pivot, $pivot_color_id, $pivot_quantity;
+    protected $listeners = ['delete'];
+
+    public function mount()
+    {
+        $this->colors = Color::all();
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $this->product->colors()->attach([
+            $this->color_id => [
+                'quantity' => $this->quantity
+            ]
+        ]);
+
+        $this->reset(['color_id', 'quantity']);
+
+        $this->emit('saved');
+
+        $this->product = $this->product->fresh();
+    }
+
+    public function edit(TbPivot $pivot)
+    {
+        $this->open = true;
+        $this->pivot = $pivot;
+        $this->pivot_color_id = $pivot->color_id;
+        $this->pivot_quantity = $pivot->quantity;
+    }
+
+    public function update()
+    {
+        $this->pivot->color_id = $this->pivot_color_id;
+        $this->pivot->quantity = $this->pivot_quantity;
+
+        $this->pivot->save();
+
+        $this->product = $this->product->fresh();
+
+        $this->open = false;
+    }
+
+    public function delete(TbPivot $pivot)
+    {
+        $pivot->delete();
+        $this->product = $this->product->fresh();
+    }
+
     public function render()
     {
-        return view('livewire.admin.color-product');
+        $productColors = $this->product->colors;
+
+        return view('livewire.admin.color-product',compact('productColors'));
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -32,7 +32,7 @@ class Product extends Model
 
     public function colors()
     {
-        return $this->belongsToMany(Color::class)->withPivot('quantity');
+        return $this->belongsToMany(Color::class)->withPivot('quantity','id');
     }
 
     public function images()
