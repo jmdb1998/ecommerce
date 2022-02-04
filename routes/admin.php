14206aebffcae0fabeafcbae6627d8288d8b1fe3
jmdb <juan.manuel.delgado.barranco@gmail.com>
2022-02-04 20:28:15 +0100

    Pdf 28: Gestion de marcas

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\ProductsController;
+use App\Http\Livewire\Admin\BrandComponent;
 use App\Http\Livewire\Admin\CreateProduct;
 use App\Http\Livewire\Admin\EditProduct;
 use App\Http\Livewire\Admin\ShowCategory;
@@ -22,3 +23,5 @@ Route::post('product/{product}/files', [ProductController::class, 'files'])->nam
 Route::get('categories', [CategoryController::class, 'index'])->name('admin.categories.index');
 
 Route::get('categories/{category}', ShowCategory::class)->name('admin.categories.show');
+
+Route::get('brands', BrandComponent::class)->name('admin.brands.index');
