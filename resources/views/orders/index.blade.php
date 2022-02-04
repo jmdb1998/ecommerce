c71569d84151b839b17b983698a9e7619ff56e89
jmdb <juan.manuel.delgado.barranco@gmail.com>
2022-02-04 20:57:50 +0100

    Pdf 29: Gestion de pedidos

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -47,71 +47,78 @@
                 </p>
             </a>
         </section>
-
-        <section class="bg-white shadow-lg rounded-lg px-12 py-8 mt-12 text-gray-700">
-            <h1 class="text-2xl mb-4">Pedidos recientes</h1>
-            <ul>
-                @foreach ($orders as $order)
-                    <li>
-                        <a href="{{ route('orders.show', $order) }}" class="flex items-center py-2 px-4 hover:bg-gray-100">
-                             <span class="w-12 text-center">
-                                 @switch($order->status)
-                                     @case(1)
-                                     <i class="fas fa-business-time text-red-500 opacity-50"></i>
-                                     @break
-                                     @case(2)
-                                     <i class="fas fa-credit-card text-gray-500 opacity-50"></i>
-                                     @break
-                                     @case(3)
-                                     <i class="fas fa-truck text-yellow-500 opacity-50"></i>
-                                     @break
-                                     @case(4)
-                                     <i class="fas fa-check-circle text-pink-500 opacity-50"></i>
-                                     @break
-                                     @case(5)
-                                     <i class="fas fa-times-circle text-green-500 opacity-50"></i>
-                                     @break
-                                     @default
-                                 @endswitch
-                             </span>
-                            <span>
-                                 Orden: {{ $order->id }}
-                                 <br>
-                                 {{ $order->created_at->format('d/m/Y') }}
-                            </span>
-                            <div class="ml-auto">
-                                 <span class="font-bold">
-                                 @switch($order->status)
+        @if($orders->count())
+            <section class="bg-white shadow-lg rounded-lg px-12 py-8 mt-12 text-gray-700">
+                <h1 class="text-2xl mb-4">Pedidos recientes</h1>
+                <ul>
+                    @foreach ($orders as $order)
+                        <li>
+                            <a href="{{ route('orders.show', $order) }}" class="flex items-center py-2 px-4 hover:bg-gray-100">
+                                 <span class="w-12 text-center">
+                                     @switch($order->status)
                                          @case(1)
-                                         Pendiente
+                                         <i class="fas fa-business-time text-red-500 opacity-50"></i>
                                          @break
                                          @case(2)
-                                         Recibido
+                                         <i class="fas fa-credit-card text-gray-500 opacity-50"></i>
                                          @break
                                          @case(3)
-                                         Enviado
+                                         <i class="fas fa-truck text-yellow-500 opacity-50"></i>
                                          @break
                                          @case(4)
-                                         Entregado
+                                         <i class="fas fa-check-circle text-pink-500 opacity-50"></i>
                                          @break
                                          @case(5)
-                                         Anulado
+                                         <i class="fas fa-times-circle text-green-500 opacity-50"></i>
                                          @break
                                          @default
                                      @endswitch
                                  </span>
-                                <br>
-                                <span class="text-sm">
-                                    {{$order->total}} &euro;
+                                <span>
+                                     Orden: {{ $order->id }}
+                                     <br>
+                                     {{ $order->created_at->format('d/m/Y') }}
                                 </span>
-                            </div>
-                            <span>
-                                <i class="fas fa-angle-right ml-6"></i>
-                            </span>
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
-        </section>
+                                <div class="ml-auto">
+                                     <span class="font-bold">
+                                     @switch($order->status)
+                                             @case(1)
+                                             Pendiente
+                                             @break
+                                             @case(2)
+                                             Recibido
+                                             @break
+                                             @case(3)
+                                             Enviado
+                                             @break
+                                             @case(4)
+                                             Entregado
+                                             @break
+                                             @case(5)
+                                             Anulado
+                                             @break
+                                             @default
+                                         @endswitch
+                                     </span>
+                                    <br>
+                                    <span class="text-sm">
+                                        {{$order->total}} &euro;
+                                    </span>
+                                </div>
+                                <span>
+                                    <i class="fas fa-angle-right ml-6"></i>
+                                </span>
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </section>
+        @else
+            <div class="bg-white shadow-lg rounded-lg px-12 py-8 mt-12 text-gray-700">
+             <span class="font-bold text-lg">
+                No existen registros de pedidos
+             </span>
+            </div>
+        @endif
     </div>
 </x-app-layout>
