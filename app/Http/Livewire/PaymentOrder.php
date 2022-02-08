72ec1a39e9e8a3ce06b712dea4a4771147f62f6b
jmdb <juan.manuel.delgado.barranco@gmail.com>
2022-02-08 17:17:48 +0100

    Arreglos para el payment

--- app/Http/Livewire/PaymentOrder.php
+++ app/Http/Livewire/PaymentOrder.php
@@ -4,7 +4,6 @@ namespace App\Http\Livewire;
 
 use App\Models\Order;
 use Livewire\Component;
-
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class PaymentOrder extends Component
@@ -12,7 +11,6 @@ class PaymentOrder extends Component
     use AuthorizesRequests;
 
     public $order;
-
     protected $listeners = ['payOrder'];
 
     public function mount(Order $order)
@@ -33,8 +31,7 @@ class PaymentOrder extends Component
         $this->authorize('view', $this->order);
 
         $items = json_decode($this->order->content);
-        $envio = json_decode($this->order->envio);
 
-        return view('livewire.payment-order', compact('items', 'envio'));
+        return view('livewire.payment-order', compact('items'));
     }
 }
