f81504a51bb047669c1659db37f3a9b73d9f7dc8
jmdb <juan.manuel.delgado.barranco@gmail.com>
2022-02-09 18:37:50 +0100

    Fin Semana 2

--- database/factories/SubcategoryFactory.php
+++ database/factories/SubcategoryFactory.php
@@ -26,7 +26,7 @@ class SubcategoryFactory extends Factory
             'slug' => Str::slug($name),
             'color' => $color,
             'size' => $color ? $size : false,
-            'image' => 'subcategories/' . $this->faker->image(storage_path('app/public/subcategories'), 640, 480, null, false)
+            /*'image' => 'subcategories/' . $this->faker->image(storage_path('app/public/subcategories'), 640, 480, null, false)*/
         ];
     }
 }
